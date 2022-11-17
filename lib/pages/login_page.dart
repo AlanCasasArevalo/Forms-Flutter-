687c284a450eddf9bcc9115130cb108d62086512
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-17 06:36:35 +0100

    Se oculta el teclado al tocar fuera del textfield

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,15 +12,20 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: [
-            TextField(
+    // Envolvemos nuestro Scaffold dentro de un GestureDetector para ocultar el teclado
+    return GestureDetector(
+      // Con esta instruccion ocultamos el teclado de manera elegante.
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.all(16),
+            children: [
+              TextField(
 
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
