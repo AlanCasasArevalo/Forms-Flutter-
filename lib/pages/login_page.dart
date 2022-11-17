a80c7af2fb15312b86b42b026988609acf9ceb73
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-17 07:07:55 +0100

    Se comienza con las validaciones de login y password

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,16 @@ class _LoginPageState extends State<LoginPage> {
   String _email = '', _password = '';
   bool _isValidEmail = false, _isValidPassword = false;
 
+  void _validateEmail() {
+    _isValidEmail = RegExp(
+            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        .hasMatch(_email);
+  }
+
+  void _validatePassword() {
+    _isValidPassword = _password.length > 9;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,27 +29,37 @@ class _LoginPageState extends State<LoginPage> {
           padding: EdgeInsets.all(16),
           children: [
             TextField(
+              key: Key('login-page-email'),
               onChanged: (emailChanged) {
                 _email = emailChanged;
               },
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(label: Text('Email')),
             ),
+            if (!_isValidEmail) Text('Email invalido'),
             SizedBox(
               height: 24,
             ),
             TextField(
+              key: Key('login-page-password'),
               onChanged: (passwordChanged) {
                 _password = passwordChanged;
               },
               obscureText: true,
               decoration: InputDecoration(label: Text('Contraseña')),
             ),
+            if (!_isValidPassword) Text('Password invalido'),
             SizedBox(
               height: 24,
             ),
             ElevatedButton(
-              onPressed: _isValidEmail && _isValidPassword ? () {} : null,
+              key: Key('login-page-Registrate-Button'),
+              onPressed: () {
+                setState(() {
+                  _validateEmail();
+                  _validatePassword();
+                });
+              },
               child: Text('Registrate'),
             ),
           ],
