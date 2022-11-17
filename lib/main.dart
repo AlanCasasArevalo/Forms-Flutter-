205642aefc0f878670775049c52c65e392a92a49
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-17 06:39:12 +0100

    Se oculta el teclado para TODOS LOS TEXTFIELDS

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    // Envolvemos nuestro Aplicacion principal dentro de un GestureDetector para ocultar el teclado en todos los textfields
+    return GestureDetector(
+      // Con esta instruccion ocultamos el teclado de manera elegante en todos los textfields.
+    onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage(),
       ),
-      home: LoginPage(),
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,20 +12,15 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Envolvemos nuestro Scaffold dentro de un GestureDetector para ocultar el teclado
-    return GestureDetector(
-      // Con esta instruccion ocultamos el teclado de manera elegante.
-      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: Scaffold(
-        body: SafeArea(
-          child: ListView(
-            padding: EdgeInsets.all(16),
-            children: [
-              TextField(
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            TextField(
 
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
