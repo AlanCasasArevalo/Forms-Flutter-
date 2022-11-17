d9fe9fbffcdbe5e109d7e347555de92c4a198b06
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-17 06:48:58 +0100

    Se agrega otro campo mas al formulario y un boton de validacion

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage({Key? key}): super(key: key);
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +16,23 @@ class _LoginPageState extends State<LoginPage> {
           padding: EdgeInsets.all(16),
           children: [
             TextField(
-
-            )
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(label: Text('Email')),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(label: Text('Contraseña')),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Registrate'),
+            ),
           ],
         ),
       ),
